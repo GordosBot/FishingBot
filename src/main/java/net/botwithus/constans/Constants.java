b9bea4692ea6d7aad52c971be23e7aaf17b149ac
Renan <renancavalcantercb@protonmail.com>
2024-06-18 16:32:11 -0300

    feat: added constants

---
+++ src/main/java/net/botwithus/constans/Constants.java
@@ -0,0 +1,8 @@
+package net.botwithus.constans;
+
+public abstract class Constants {
+    public static final String NET = "Net";
+    public static final String BAIT = "Bait";
+
+    public static final String DRAYNOR_SHRIMP = "00";
+}

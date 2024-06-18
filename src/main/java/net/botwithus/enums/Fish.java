1d9c465b6acd3ce0e97860f465007bcb65d2f080
Renan <renancavalcantercb@protonmail.com>
2024-06-18 16:33:08 -0300

    feat: added logic to use code instead string

--- src/main/java/net/botwithus/enums/Fish.java
+++ src/main/java/net/botwithus/enums/Fish.java
@@ -1,19 +1,28 @@
 package net.botwithus.enums;
 
+import net.botwithus.constans.Constants;
+
 import java.util.Arrays;
 
 public enum Fish {
 
-    SHRIMP("Raw shrimps", "Draynor Village"),
-    CRAYFISH("Raw crayfish", "Lumbridge"),
-    TROUT("Raw trout", "Lumbridge");
+    SHRIMP("Raw shrimps", "Draynor Village", Constants.NET, "", "00"),
+    SARDINE("Raw sardine", "Draynor Village", Constants.NET, "Fishing bait", "01"),
+    CRAYFISH("Raw crayfish", "Lumbridge", Constants.BAIT, "", "10"),
+    TROUT("Raw trout", "Lumbridge", Constants.BAIT, "Feather", "11");
 
     private String npcName;
     private String area;
+    private String fishingMethod;
+    private String bait;
+    private String code;
 
-    Fish(String npcName, String area) {
+    Fish(String npcName, String area, String fishingMethod, String bait, String code) {
         this.npcName = npcName;
         this.area = area;
+        this.fishingMethod = fishingMethod;
+        this.bait = bait;
+        this.code = code;
     }
 
     public String getNpcName() {
@@ -22,6 +31,18 @@ public enum Fish {
 
     public String getArea() {
         return area;
+    }
+
+    public String getFishingMethod() {
+        return fishingMethod;
+    }
+
+    public String getBait() {
+        return bait;
+    }
+
+    public String getCode() {
+        return code;
     }
 
     public static String[] getAreas() {
@@ -36,5 +57,12 @@ public enum Fish {
                 .filter(fish -> fish.getArea().equals(area))
                 .map(Enum::name)
                 .toArray(String[]::new);
+    }
+
+    public static Fish getByCode(String code) {
+        return Arrays.stream(values())
+                .filter(fish -> fish.getCode().equals(code))
+                .findFirst()
+                .orElse(null);
     }
 }
